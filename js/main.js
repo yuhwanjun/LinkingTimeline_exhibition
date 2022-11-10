a08e907a0a1263934b2234f23f85669e3f557897
yuhwanjun <yuhwanjun0311@gmail.com>
2022-11-11 03:44:29 +0900

    default settings
    default settings

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
 import '../scss/main.scss';
 import '../js/three.js';
 
-import gsap from "gsap";
-
--- js/three.js
+++ js/three.js
@@ -1,46 +1,103 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from "gsap";
 
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
-const scene = new THREE.Scene()
+/*
+    scene
+*/
+const scene = new  THREE.Scene();
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+/*
+    object
+*/
+const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-
 scene.add(mesh)
 
-// Sizes
+
+/*
+    camera
+*/
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
 camera.position.z = 3
+camera.lookAt(mesh.position)
 scene.add(camera)
 
+/*
+    mousemove
+*/
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+    // console.log(event.x, event.y)
+})
+
+/*
+    renderer
+*/
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+renderer.setSize(sizes.width, sizes.height)
 
+/*
+    controls
+*/
+const controls = new OrbitControls(camera, canvas)
+controls.target.y = 2
+controls.enableDamping = true
+
+
+/*
+    animaition
+*/
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
+    // camera.position.y = cursor.y * 3
+    controls.update()
+    camera.lookAt(mesh.position)
+
+    mesh.rotation.y = elapsedTime
+    renderer.render(scene, camera)
+
+    
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
+/*
+    resize
+*/
 window.addEventListener('resize', () =>
 {
-    // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
-
-    // Update camera
+    
     camera.aspect = sizes.width / sizes.height
     camera.updateProjectionMatrix()
 
-
-    // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
-
-renderer.render(scene, camera)
