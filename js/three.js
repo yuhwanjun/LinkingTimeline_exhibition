38b76889c4a1b92152c6dc09098e6cf0eaf80034
yuhwanjun <yuhwanjun0311@gmail.com>
2022-11-11 12:16:40 +0900

    Update three.js

--- js/three.js
+++ js/three.js
@@ -10,14 +10,68 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new  THREE.Scene();
 
 
+
 /*
     object
 */
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
 
+
+// create box****************************
+// const w = 1000, h = 700;
+// const Lx = 55, N1x = 435, Gx = 20;
+// const Ly = 138.5, Ny = Ly-61, Gy= 174 ;
+const w = 1000, h = 700;
+const Lx = 0, N1x = 630, Gx = 0;
+const Ly = 138.5, Ny = Ly-61, Gy= 174 ;
+
+
+const shape = new THREE.Shape();
+shape.moveTo( 0,0 );
+
+shape.lineTo( Lx,0 );
+shape.lineTo( Lx,Ly );
+shape.lineTo( Lx+165,Ly );
+shape.lineTo( Lx+165,Ny );
+
+shape.lineTo( N1x,Ny );
+shape.lineTo( N1x,Ly );
+shape.lineTo( N1x+205,Ly );
+shape.lineTo( N1x+205,Ny );
+
+shape.lineTo( w-Gx-127-35,Ny );
+shape.lineTo( w-Gx-127-35,Ly );
+shape.lineTo( w-Gx-127,Ly );
+shape.lineTo( w-Gx-127,Gy );
+shape.lineTo( w-Gx,Gy );
+shape.lineTo( w-Gx,0 );
+shape.lineTo( w,0 );
+
+shape.lineTo( w,h );
+shape.lineTo( 0,h );
+shape.lineTo( 0,0 );
+
+const extrudeSettings = {
+	steps: 2,
+	depth: 16,
+	bevelEnabled: true,
+	bevelThickness: 1,
+	bevelSize: 1,
+	bevelOffset: 0,
+	bevelSegments: 1
+};
+
+const geometry2 = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+const material2 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const mesh2 = new THREE.Mesh( geometry2, material2 ) ;
+scene.add( mesh2 );
+
+mesh2.rotation.x = Math.PI * 1;
+mesh2.position.x = -500;
+mesh2.position.y = 350;
 
 /*
     camera
@@ -27,9 +81,9 @@ const sizes = {
     height: window.innerHeight
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
-camera.position.z = 3
-camera.lookAt(mesh.position)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100000)
+camera.position.z = 1000
+camera.lookAt(mesh2.position)
 scene.add(camera)
 
 /*
@@ -70,15 +124,15 @@ const clock = new THREE.Clock()
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    // const elapsedTime = clock.getElapsedTime()
 
     // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
     // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
     // camera.position.y = cursor.y * 3
     controls.update()
-    camera.lookAt(mesh.position)
+    // camera.lookAt(mesh2.position)
 
-    mesh.rotation.y = elapsedTime
+    // mesh.rotation.y = elapsedTime
     renderer.render(scene, camera)
 
     
